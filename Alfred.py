846ed3cd2cec6bce5b00c971e32c27762fec1567
SaintLaurel <ceruta.laurentiu@gmail.com>
2023-11-02 22:34:59 +0200

    Initial commit

---
+++ Alfred.py
@@ -0,0 +1,19 @@
+import pyautogui
+import time
+
+def main():
+    x, y = 200, 200
+    blu = pyautogui.pixel(x, y)
+    pyautogui.moveTo((x, y), duration=0.001)
+    pyautogui.click()
+
+    while True:
+        click = pyautogui.pixel(x, y)
+
+        if click == (30, 151, 80):
+            pyautogui.click()
+            time.sleep(0.1)
+            pyautogui.click()
+
+if __name__ == "__main__":
+    main()
