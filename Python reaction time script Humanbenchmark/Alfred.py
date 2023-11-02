3bffb751c0d3ce254237edd89685ea55febf2299
SaintLaurel <149721433+SaintLaurel@users.noreply.github.com>
2023-11-03 00:07:55 +0200

    Alfred.py

--- Python reaction time script Humanbenchmark/Alfred.py
+++ Python reaction time script Humanbenchmark/Alfred.py
@@ -3,14 +3,13 @@ import time
 
 def main():
     x, y = 200, 200
-    blu = pyautogui.pixel(x, y)
     pyautogui.moveTo((x, y), duration=0.001)
     pyautogui.click()
 
     while True:
-        click = pyautogui.pixel(x, y)
+        colour = pyautogui.pixel(x, y)
 
-        if click == (30, 151, 80):
+        if colour == (30, 151, 80):
             pyautogui.click()
             time.sleep(0.1)
             pyautogui.click()
